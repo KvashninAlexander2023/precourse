37bf80c19bf542fc5e50869d209d43bd42a35b24
KvashninAlexander2023 <study_python@mail.ru>
2023-11-18 00:10:36 +0500

    les03task01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,12 @@
-// code
+const colors = ['синий', 'черный', 'зеленый', 'красный', 'желтый', 'зеленый'];
+
+function createColorString() {
+  // return colors ФИЛЬТРУЕМ.ПРЕВРАЩАЕМ В СТРОКУ('-')
+  return colors
+    .filter((elem) => {
+      return elem === 'черный' || elem === 'красный' || elem === 'желтый';
+    })
+    .join('-');
+}
+
+console.log(createColorString());
